d4f5da4c9307799614c785b5a4554a9bd077b14c
Chukwunonso Nwankpa <chukwunonsonwankpa@gmail.com>
2024-05-25 13:47:25 +0100

    Improve storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,3 +68,22 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        """Retrieves one object"""
+        objects = self.all(cls)
+        for object in objects.values():
+            if object.id == id:
+                return object
+            else:
+                return None
+        return None
+
+    def count(self, cls=None):
+        """Count the number of objects in storage"""
+        objects = self.all(cls)
+        count = 0
+        for object in self.all(cls).values():
+            count+= 1
+        return count
+        pass
