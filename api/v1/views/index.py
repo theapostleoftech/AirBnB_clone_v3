dcab80fd931da7f0d5e0b578445b711b72affff5
Chukwunonso Nwankpa <chukwunonsonwankpa@gmail.com>
2024-05-26 20:05:27 +0100

    Some stats?

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """This contains all API endpoints"""
 from api.v1.views import app_views
+from models import storage
 
 from flask import jsonify
 
@@ -9,3 +10,9 @@ from flask import jsonify
 def status_route():
     """This endpoint returns the status of the API"""
     return jsonify({"staus": "OK"})
+
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def stats_route():
+    """This endpoint retrieves the number of each objects by type"""
+    return storage.count()
